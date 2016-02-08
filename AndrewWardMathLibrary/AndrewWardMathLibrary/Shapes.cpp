2886d27aacda75194d4585725ae593fc3d24c708
Andrew Ward <drewwardgov@gmail.com>
2016-02-08 13:39:52 -0800

    adding shapes

---
+++ AndrewWardMathLibrary/AndrewWardMathLibrary/Shapes.cpp
@@ -0,0 +1,11 @@
+#include "Shapes.h"
+
+andMath::ConvexHull andMath::operator*(const mat3 & matrix, const ConvexHull & shape)	{ return matrix * shape; }
+
+andMath::Ray andMath::operator*(const mat3 & matrix, const Ray & shape)					{ return matrix * shape; }
+
+andMath::Plane andMath::operator*(const mat3 & matrix, const Plane & shape)				{ return matrix * shape; }
+
+andMath::Circle andMath::operator*(const mat3 & matrix, const Circle & shape)			{ return matrix * shape; }
+
+andMath::AABB andMath::operator*(const mat3 & matrix, const AABB & shape)				{ return matrix * shape; }
